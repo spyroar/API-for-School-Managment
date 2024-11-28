7f61c56e66588eec8e88dcfb8a35f1693bd6ea4f
Shivpal <yadavshivpal1999@gmail.com>
2024-11-28 11:52:41 +0530

    add error fixed

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const app = express();
 var bodyParser = require("body-parser");
 const router = require("./routes/schoolRoute");
 const db = require("./db/dbConnection");
+const cors = require("cors");
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
+app.use(cors());
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
-app.use(router)
-app.get('/',(req,res)=>{
-     res.status(200).send("Please visit on <a>https://api-for-school-managment.vercel.app/listSchools?latitude=26.764177&longitude=83.076775</a>");
-})
+app.use(router);
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send(
+      "Please visit on <a>https://api-for-school-managment.vercel.app/listSchools?latitude=26.764177&longitude=83.076775</a>"
+    );
+});
 app.listen(port, () => {
-
   console.log(`Server listening on port ${port}`);
 });
-
 
 // shiv123 shivpalyadav
